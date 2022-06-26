de1728a48d7a9059bef4109ab78384be7df8bd7a
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-06-26 17:58:35 +0200

    Set up basic structure for the game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
 import socketio from 'socket.io-client'
-import Timer from './components/Timer';
+import GameBoard from './pages/GameBoard'
+import NotFound from './pages/NotFound'
+import StartPage from './pages/StartPage'
 import './App.css';
 
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
 
 const App = () => {
+	console.log(socket)
 	return (
-		<div className="container text-center">
-			<Timer socket={socket} />
+		<div id='App'>
+			<Routes>
+				<Route path='/' element={<StartPage />} />
+				<Route  path='/games/:game_id' element={<GameBoard />} />
+				<Route  path='*' element={<NotFound />} />
+			</Routes>
 		</div>
 	)
 }
